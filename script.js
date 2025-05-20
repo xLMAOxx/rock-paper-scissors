e4f39ba72c1f0db88ec55e8ab74bf35967e769a4
fati.Cis <Fatumatacissse24@gmail.com>
2025-05-20 20:47:44 +0200

    added some effects that are still being edited

--- script.js
+++ script.js
@@ -7,6 +7,40 @@ const resultDiv = document.getElementById("result");
 const scoreDiv = document.getElementById("score");
 const gameResultDiv = document.getElementById("gameResult");
 const InterfaceDiv = document.getElementById("interface");
+let originalDOM = document.body.cloneNode(true);
+
+
+/*function resetDOM() {
+    document.body.replaceWith(originalDOM.cloneNode(true))
+};*/
+function winner(){
+    InterfaceDiv.textContent = ""; 
+    const newDiv = document.createElement("div"); 
+    newDiv.style.fontFamily = "Verdana, Geneva, Tahoma, sans-serif"; 
+    newDiv.style.fontSize = "50px"; 
+    newDiv.style.color = "#B95F89";
+    newDiv.style.textAlign = "center";
+    newDiv.style.paddingTop = "2.5em";
+
+    InterfaceDiv.appendChild(newDiv); 
+
+    if(win> lost){
+        console.log("YOU HAVE WON THE GAME!");
+        newDiv.textContent = "YOU HAVE WON THE GAME!";
+        document.body.style.backgroundColor = "#5ac25d";
+            
+    } else if(lost > win){
+      console.log("YOU HAVE LOST THE GAME!");
+      newDiv.textContent = "YOU HAVE LOST THE GAME!";
+      document.body.style.backgroundColor = "#c25d5a";
+     // document.body.style.color = "blue"; // Changes all text color to blue
+
+      // document.documentElement.style.setProperty("--text-color", "black");
+    }else{
+        console.log("ITS A TIE!");
+       newDiv.textContent = "ITS A TIE!";
+    }
+};
 
 
 document.getElementById("choices").addEventListener("click", function(event){
@@ -21,47 +55,34 @@ document.getElementById("choices").addEventListener("click", function(event){
     botEndResult = "BOT CHOSE " + botIndex;
 
     function gamestart(){
+        const divsize= "3em";
+        bothChoicesDiv.style.fontSize = divsize;
         if(botIndex === elementId){
-            bothChoicesDiv.innerText= `Bot chose ${botIndex} and you chose ${elementId}`;
+            bothChoicesDiv.innerText= ` ${botIndex} VS ${elementId}`;
             resultDiv.innerText = "TIE!";
-            return tie +=1;
-        } else if(elementId === "rock" && botIndex === "paper" || elementId === "paper" && botIndex=== "rock"|| elementId === "scissors" && botIndex === "paper"){
-            bothChoicesDiv.innerText= `BOT CHOSE ${botIndex.toUpperCase()} AND YOU CHOSE ${elementId.toUpperCase()}`;
+            tie +=1;
+        } else if(elementId === "paper" && botIndex === "rock" || elementId === "rock" && botIndex=== "scissors"|| elementId === "scissors" && botIndex === "paper"){
+            bothChoicesDiv.innerText= ` ${botIndex.toUpperCase()} VS ${elementId.toUpperCase()}`;
             resultDiv.innerText = "YOU WIN!";
-            return win +=1;
+            win +=1;
         } else{
-            bothChoicesDiv.innerText= `Bot chose ${botIndex} and you chose ${elementId}`;
+            bothChoicesDiv.innerText= ` ${botIndex} VS ${elementId}`;
             resultDiv.innerText = "YOU LOSE!";
-            return lost +=1;
+            lost +=1;
         }
 
     }
     gamestart();
     scoreDiv.innerText = `WINS: ${win} | LOST: ${lost} | TIES: ${tie}`;
 });
-
-    function winner(){
-        InterfaceDiv.textContent = ""; 
-        const newDiv = document.createElement("div"); 
-        newDiv.style.fontFamily = "Verdana, Geneva, Tahoma, sans-serif"; 
-        newDiv.style.fontSize = "50px"; 
-        newDiv.style.color = "#B95F89";
-        newDiv.style.textAlign = "center"
-        newDiv.padding = "2em";
-        InterfaceDiv.appendChild(newDiv); 
-
-        if(win> lost){
-            console.log("YOU HAVE WON THE GAME!");
-            newDiv.textContent = "YOU HAVE WON THE GAME!";
-            
-        } else if(lost > win){
-            console.log("YOU HAVE LOST THE GAME!");
-            newDiv.textContent = "YOU HAVE LOST THE GAME!";
-        }else{
-            console.log("ITS A TIE!");
-           newDiv.textContent = "ITS A TIE!";
-        }
-    };
 document.getElementById("end").addEventListener("click", function(){
-winner();
+    winner();
+});
+
+document.getElementById("reset").addEventListener("click", function(){
+    win = 0;
+    lost = 0;
+    tie = 0;
+    location.reload();
+
 });
